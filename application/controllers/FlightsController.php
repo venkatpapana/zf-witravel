80c0da7a5a9122863178f2fa909fd903321fc077
Papana, VenkataRao <VenkataRao.Papana@cdk.com>
2016-04-28 22:26:22 +0530

    Air Reservation integration : in-progress

--- application/controllers/FlightsController.php
+++ application/controllers/FlightsController.php
@@ -55,14 +55,43 @@ class FlightsController extends WiTravelBaseController
     public function flightReservationAction() {
         // $wiconfig = Zend_Controller_Front::getInstance()->getParam('bootstrap')->getOption('wiconfig');
         //
-        // //TODO: read the req params
-        $search = new FlightSearchCriteria();
-        // $search->setFromPlace('AMS');
-        // $search->setFromDate('2016-04-20');
-        // $search->setToDate('2016-04-22');
-        // $search->setToPlaces($wiconfig['Destinations']);
-        // $search->setBudget('500');
-        // $search->setNumPassengers(2);
+
+        //TODO: read the req params
+        $reservation = new FlightReservationCriteria();
+        $reservation->hotelCode = 53495;
+        $reservation->numAdults = 2;
+        $reservation->checkinDate = '2016-05-20';
+        $reservation->checkoutDate = '2016-05-22';
+        $reservation->numRooms = '1';
+
+        //traveller
+        $traveller = new Traveller('Mr','John','Smith','40','1976-01-21','M','US','johnsmith@travelportuniversalapidemo.com');
+
+        //delivery address
+        $shippingAddr = new Address();
+        $shippingAddr->street = 'Via Augusta 59 5';
+        $shippingAddr->city = 'Madrid';
+        $shippingAddr->postalCode = '50156';
+        $traveller->shippingAddress = $shippingAddr;
+
+        //address
+        $addr = $shippingAddr;
+        $addr->name = 'DemoSiteAddress';
+        $addr->state = 'IA';
+        $addr->postalCode = '50156';
+        $addr->country = 'US';
+        $traveller->address = $addr;
+
+
+        //phone
+        $phone = new Phone('DEN', '1', '303', '123456789');
+        $traveller->phone = $phone;
+
+        //credit card
+        $cc = new CreditCard('VI', '4444333322221111', '2016-12', '123');
+        $traveller->creditCard = $cc;
+
+        $reservation->traveller = $traveller;
 
 
         //send request
